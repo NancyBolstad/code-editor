f7b248fc0887a1064fe91e09db53dc20790183b4
NancyBolstad <litchacha@gmail.com>
2019-04-28 19:31:27 +0200

    Added a random integer generation function

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { constAndLet } from './examples';
+import { arrowFunctions } from './examples';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 const shakespeareApi = 'https://api.graph.cool/simple/v1/shakespeare';
 
-let options = {
+let options = ()=> {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json'
@@ -8,3 +8,14 @@ let options = {
   //The JSON.stringify() method converts a JavaScript object or value to a JSON string, optionally replacing values if a replacer function is specified or optionally including only the specified properties if a replacer array is specified.
   body: JSON.stringify({})
 };
+
+function randomIt(min,max){
+    //The Math.ceil() function returns the smallest integer greater than or equal to a given number.
+    //Note: Math.ceil(null) returns integer 0 and does not give a NaN error.
+    min=Math.ceil(min);
+    //The Math.floor() function returns the largest integer less than or equal to a given number.
+    //Note: Math.floor(null) returns 0, not a NaN error.
+    max=Math.floor(max);
+
+    return Math.floor(Math.random()*(max-min+1))+min
+}
