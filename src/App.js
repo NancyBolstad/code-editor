842c0b667fc8cb668909392372f3d3c12052c1b6
NancyBolstad <litchacha@gmail.com>
2019-04-28 21:10:20 +0200

    Attached a newFields function to the New Rule button

--- src/App.js
+++ src/App.js
@@ -14,7 +14,12 @@ import {
 
 class App extends Component {
   state = {
-    editor: ''
+    editor: '',
+    name0: '',
+    begin0: '',
+    end0: '',
+    style0: '',
+    rules: 1
   };
 
   handleChange = event => {
@@ -23,17 +28,44 @@ class App extends Component {
       //computed property name
       [name]: value
     });
-    console.log(this.state);
+  };
+
+  newFields = () => {
+    this.setState(prevState => {
+      //pull the number of rules from previous state
+      let { rules } = prevState;
+      //loop through the fields when creating new field
+      let fields = ['name', 'begin', 'end', 'style'];
+      //take in all of the input values
+      let inputValues = {};
+      //loop through the fields, this fields.forEach is going to create each of the fields
+      fields.forEach(field => {
+        inputValues = {
+          //use spread operator to copy and paste all of the old input values into the new inputValue
+          ...inputValues,
+          //for new ones, use the computed property name to pu in the particular field and the number of rules that the application had so far
+          [`${field}${rules}`]: ''
+        };
+      });
+      //Just to make sure that I am incrementing the rules because we want the next time we create a rule for it to have a different index value than the past one. So we need to make sure we increment that
+      rules++;
+      //the setState function expects us to return an object that will represent the changes we are making to the state
+      return {
+        rules,
+        //use the object assembly shorthand
+        ...inputValues
+      };
+    });
   };
 
   render() {
     let { value } = this.state;
-    let { handleChange } = this;
+    let { handleChange, newFields } = this;
 
     return (
       <Container>
         <Column>
-          <Button>New Rule</Button>
+          <Button onClick={newFields}>New Rule</Button>
         </Column>
         <Column>
           <Button>Random Text</Button>
